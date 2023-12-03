a49f832d85b102b612ee79d163d740691eadf6e5
Rubydog <hirabizz@gmail.com>
2023-12-03 16:13:33 +0900

    追加: スプラッシュ画面

--- lib/providers/ingredient_provider.dart
+++ lib/providers/ingredient_provider.dart
@@ -3,7 +3,8 @@ import 'package:riverpod_annotation/riverpod_annotation.dart';
 import 'package:kondate_app/services/api_service.dart';
 part 'ingredient_provider.g.dart';
 
-@riverpod
+// スプラッシュ画面で準備したデータを残しておくために keepAlive する
+@Riverpod(keepAlive: true)
 class IngredientNotifier extends _$IngredientNotifier {
   Map<int, Ingredient?> ingredientMap = {};
 
